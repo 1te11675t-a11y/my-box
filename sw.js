5de91dce0f489670fad589f44e8facd1a8ba24a0
1te11675t-a11y <1te11675t@gmail.com>
2026-08-31 15:54:52 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+/* 圏外でも開けるようにするための、最小限のサービスワーカー。
+   ページ本体は「まずネットワーク、だめならキャッシュ」。
+   更新したファイルをGitHubに上げれば、次にオンラインで開いたときに反映されます。 */
+
+const CACHE = "moving-boxes-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./qrcode.js",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  if (req.method !== "GET" || new URL(req.url).origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(req)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then(hit => hit || caches.match("./index.html")))
+  );
+});
